63d2241b06b77297311f585d22239be1b27ac148
Ruben-JR <rubenpina758@gmail.com>
2022-10-11 08:10:06 -0100

    create components board and game

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,23 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    render () {
-        return (
-            <button className="square">
-                { /* TODO */}
-            </button>
-        );
-    }
+function Square(props) {
+    return(
+        <button className='square' onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
 }
 
 class Board extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+            xIsNext: true,
+        };
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
+    }
+
     renderSquare(i) {
-        return <Square />;
+        return (
+            <Square value = {this.state.squares[i]} onClick={() => this.handleClick(i)}/>
+        );;
     }
 
     render() {
-        const status = 'Next player: X';
+        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
         return (
             <div>
@@ -57,6 +74,19 @@ class Game extends React.Component {
             </div>
         );
     }
+}
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
